b6abf6c1911ec0f0e8e7e26ec0ef21ee2ddd963e
Алексей <@DESKTOP-3APITBG>
2023-11-21 20:46:52 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string login = txtLogin.Text;
+            string password = txtPassword.Password;
+
+
+            if (login == "admin" && password == "1234")
+            {
+                MessageBox.Show("Вход выполнен успешно");
+            }
+            else
+            {
+                Exaption.Text = "Неправильный  логин или пароль";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string gender = cmbGender.Name;
+            string password = txtRegPassword.Password;
+            string confirmPassword = txtRegPasswordConfirm.Password;
+
+            if (password == confirmPassword)
+            {
+                MessageBox.Show("Регистрация выполнена успешно");
+            }
+            else
+            {
+                MessageBox.Show("Пароли не совпадают");
+            }
+        }
+    }
+}
